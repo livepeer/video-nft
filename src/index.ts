6b1515132ee34b3a933eb5efb1a2ec05e0d04e29
Victor Elias <victor@livepeer.org>
2022-02-28 16:03:08 +0000

    Transcode too large assets to 100MB
    Still needs to have an enquirer to give the option to
    users not to transcode in case they don't want it.
    
    In the future, we might also want to create an NFT with
    only the animation_url set to the downscaled video.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
 import parseCli from './cli';
 import VodApi from './api';
+import { Asset, FfmpegProfile } from './types/schema';
+
+const openSeaNftSizeLimit = 100_000_000; // 100 MB
+
+function getDesiredProfile(asset: Asset): FfmpegProfile | null {
+	const size = asset.size ?? 0;
+	const videoTrack = asset.videoSpec?.tracks?.find(t => t.type === 'video');
+	const { bitrate, width, height } = videoTrack ?? {};
+	if (size <= openSeaNftSizeLimit || !bitrate || !width || !height) {
+		return null;
+	}
+
+	const audioTrack = asset.videoSpec?.tracks?.find(t => t.type === 'audio');
+	const audioBitrate = audioTrack?.bitrate ?? 0;
+	const desiredBitrate = Math.floor(
+		(bitrate + audioBitrate) * (openSeaNftSizeLimit / size) - audioBitrate
+	);
+	// We only change the resolution if the bitrate changes too much. We don't go
+	// below 720p though since the bitrate is the thing that really matters. We
+	// don't need to handle aspect ratio since go-livepeer will do it for us.
+	const referenceWidth = width * Math.sqrt(desiredBitrate / bitrate);
+	const changeResolution = width > 1280 && referenceWidth < 1280;
+	return {
+		name: 'nft',
+		bitrate: desiredBitrate,
+		width: changeResolution ? 1280 : width,
+		height: changeResolution ? 720 : height,
+		fps: 0
+	};
+}
 
 async function videoNft() {
 	const args = await parseCli();
@@ -8,14 +38,27 @@ async function videoNft() {
 	console.log('1. Requesting upload URL... ');
 	const {
 		url: uploadUrl,
-		asset,
+		asset: pendingAsset,
 		task: importTask
 	} = await api.requestUploadUrl(args.assetName);
-	console.log(`Pending asset with id=${asset.id}`);
+	console.log(`Pending asset with id=${pendingAsset.id}`);
 
 	console.log('2. Uploading file...');
 	await api.uploadFile(uploadUrl, args.filename as string);
 	await api.waitTask(importTask);
+
+	let asset = await api.getAsset(pendingAsset.id ?? '');
+	const desiredProfile = getDesiredProfile(asset);
+	if (desiredProfile) {
+		console.log(
+			`3. Transcoding asset to ${desiredProfile.width} x ${
+				desiredProfile.height
+			} @ ${Math.round(desiredProfile.bitrate / 1024)} kbps bitrate`
+		);
+		const transcode = await api.transcodeAsset(asset, desiredProfile);
+		await api.waitTask(transcode.task);
+		asset = transcode.asset;
+	}
 
 	console.log('3. Starting export... ');
 	let { task: exportTask } = await api.exportAsset(
